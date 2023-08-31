1d676151693ab32c1c9aceb4b419125200780a2b
OnojaPeter <onojapeter90@gmail.com>
2023-08-31 19:57:15 +0100

    update on body

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 0,
-    nav: true,
-    dots: false,
-    autoplay: 2000,
-    navText: ["<i class = 'fa fa-chevron-left'></i>", "<i class = 'fa fa-chevron-right'></i>"],
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 1
-      },
-      1000: {
-        items: 1
-      }
-    }
-  })
+import express from "express";
+
+
+// import jsdom from "jsdom";
+// const dom = new jsdom.JSDOM("");
+// import jQuery from "jquery" dom.window;
+
+// const express = require("express");
+
+// const { JSDOM } = require( "jsdom" );
+// const { window } = new JSDOM( "" );
+// const $ = require( "jquery" )( window );
+
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/aboutUs", (req, res) => {
+  res.render("aboutUs.ejs");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact.ejs");
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
