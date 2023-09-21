8daf0a9408b8121921ad429323e55a732f5abd1e
OnojaPeter <onojapeter90@gmail.com>
2023-09-21 12:42:46 +0100

    done with the js of hamburger

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+const items = document.getElementById("menuItems");
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+
+// openMenu.addEventListener("click", () => {
+//   if (items.style.display !="flex"){
+//   items.style.display = "flex";
+//   closeMenu.style.display = "block"
+//   openMenu.style.display = "none"
+//   } else {
+//   items.style.display = "none";
+//   }
+// });
+
+// closeMenu.addEventListener("click", () => {
+//   if (items.style.display !="none"){
+//   items.style.display = "none";
+//   closeMenu.style.display = "none";
+//   openMenu.style.display = "block";
+//   } else {
+      
+//   }
+// });
+
+openMenu.addEventListener("click", () => {
+  document.getElementById("mySidenav").style.width = "55%";
+  $("#body").addClass("scroll");
+});
+
+closeMenu.addEventListener("click", () => {
+  document.getElementById("mySidenav").style.width = "0";
+  $("#body").removeClass("scroll");
+});
+
+
+
+
+
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 0,
